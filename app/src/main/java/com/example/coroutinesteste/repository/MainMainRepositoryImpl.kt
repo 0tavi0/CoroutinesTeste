8496487b99efd9fcac3705bc3ad1b9d9584ac038
Otavio <otavio_leao@hotmail.com>
2020-04-06 17:00:56 -0300

    Config api key to gradle

--- app/src/main/java/com/example/coroutinesteste/repository/MainMainRepositoryImpl.kt
+++ app/src/main/java/com/example/coroutinesteste/repository/MainMainRepositoryImpl.kt
@@ -1,12 +1,13 @@
 package com.example.coroutinesteste.repository
 
+import com.example.coroutinesteste.BuildConfig
 import com.example.coroutinesteste.BuildConfigHelper
 import com.example.coroutinesteste.domain.response.TrendingMoviesResponse
 import com.example.coroutinesteste.service.ApiService
 
 class MainMainRepositoryImpl(private val apiService: ApiService) : MainRepository {
     override suspend fun getMovies(): TrendingMoviesResponse {
-        return apiService.getTrendingMovies(BuildConfigHelper.API_KEY)
+        return apiService.getTrendingMovies(BuildConfig.API_KEY)
 
 //    override suspend fun getMovies(): List<Movie> {
 //        return withContext(Dispatchers.Default) {
